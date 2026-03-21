7c77ce357b25bf2a8d8c2ee9fd9fc8c8f75fd677
Riteek Doble <pawarriteek78@gmail.com>
2026-03-21 12:27:14 +0530

    first commit

---
+++ src/test/java/com/email/Smart_Email_Assistant/SmartEmailAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.Smart_Email_Assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEmailAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
